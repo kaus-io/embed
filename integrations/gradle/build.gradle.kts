93e1b4fb55a8f6e8b97a95666e59400ce02ba344
ZXHHYJ <957447668@qq.com>
2026-05-21 06:55:16 +0800

    build: 按 Kotlin 官方教程配置项目 Maven 发布逻辑

--- integrations/gradle/build.gradle.kts
+++ integrations/gradle/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 kotlin {
@@ -24,6 +25,37 @@ gradlePlugin {
             displayName = "Embed Gradle Plugin"
             description = "Runs the code generation process for Embed"
             implementationClass = "com.zxhhyj.embed.gradle.EmbedPlugin"
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(group.toString(), "embed-gradle-plugin", version.toString())
+    pom {
+        name = "embed-gradle-plugin"
+        description = "Gradle plugin for Embed - integrates resource generation into the build"
+        inceptionYear = "2026"
+        url = "https://github.com/zxhhyj/embed"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
+                distribution = "https://opensource.org/licenses/MIT"
+            }
+        }
+        developers {
+            developer {
+                id = "zxhhyj"
+                name = "zxhhyj"
+                url = "https://github.com/zxhhyj"
+            }
+        }
+        scm {
+            url = "https://github.com/zxhhyj/embed"
+            connection = "scm:git:git://github.com/zxhhyj/embed.git"
+            developerConnection = "scm:git:ssh://github.com/zxhhyj/embed.git"
         }
     }
 }
